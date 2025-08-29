6d655f4a7436a2a2d3493805929b17caee0daf05
CmdrCat <cgifaction@gmail.com>
2025-08-29 17:02:54 -0500

    Updated print name

--- lua/weapons/doi_atow_webley/shared.lua
+++ lua/weapons/doi_atow_webley/shared.lua
@@ -10,7 +10,7 @@ include("sh_soundscript.lua")
 
 if CLIENT then
 	SWEP.DrawCrosshair = false
-	SWEP.PrintName = "Webley MkIV"
+	SWEP.PrintName = "Webley Mk IV"
 	SWEP.CSMuzzleFlashes = true
 	SWEP.UseHands = true
 	
