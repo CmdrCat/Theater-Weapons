ab213403830ee154f715c663b0b2503c1c47b841
CmdrCat <cgifaction@gmail.com>
2025-09-01 21:49:29 -0500

    Added Glock FUN SWITCH attachment

---
+++ lua/cw/shared/attachments/uecw_glock_switch.lua
@@ -0,0 +1,28 @@
+local att = {}
+att.name = "uecw_glock_switch"
+att.displayName = "Glock Switch"
+att.displayNameShort = "Switch"
+
+att.statModifiers = {FireDelayMult = -1 / 3}
+
+if CLIENT then
+	att.displayIcon = surface.GetTextureID("atts/fullautorec")
+	att.description = {[1] = {t = "Adds select-fire capability; good luck hitting anything", c = CustomizableWeaponry.textColors.VPOSITIVE},
+[2] = {t = "Change fire mode by pressing E + R", c = CustomizableWeaponry.textColors.POSITIVE}}
+end
+
+function att:attachFunc()
+	self:CycleFiremodes() 
+	self.FireModes = {"semi","safe","auto"}
+	self:CycleFiremodes()
+	self:CycleFiremodes()
+end
+
+function att:detachFunc()
+	self:CycleFiremodes()
+	self.FireModes = {"semi","safe"}
+	self:CycleFiremodes()
+	self:CycleFiremodes()
+end
+
+CustomizableWeaponry:registerAttachment(att)
