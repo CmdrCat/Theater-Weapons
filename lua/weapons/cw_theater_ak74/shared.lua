9fe4d743f9930e0f01c2b7baa3411863eedbca8d
NotTrashraD <bellie1579@gmail.com>
2025-09-02 03:37:59 +0200

    romeo4t

--- lua/weapons/cw_theater_ak74/shared.lua
+++ lua/weapons/cw_theater_ak74/shared.lua
@@ -14,8 +14,11 @@ if CLIENT then
 	SWEP.AimpointPos = Vector(-3.481, 0, -0.16)
 	SWEP.AimpointAng = Vector(0, 0, 0)
 	
-SWEP.EXPSPos = Vector(-3.57, 0, -0.48)
-SWEP.EXPSAng = Vector(0, 0, 0)
+	SWEP.EXPSPos = Vector(-3.57, 0, -0.48)
+	SWEP.EXPSAng = Vector(0, 0, 0)
+
+	SWEP.ROMEO4TPos = Vector(-3.57, 0, -0.52)
+	SWEP.ROMEO4TAng = Vector(0, 0, 0)
 
 	
 	SWEP.KobraPos = Vector(-3.641, -4.074, 0.159)
@@ -55,8 +58,8 @@ SWEP.EXPSAng = Vector(0, 0, 0)
 	SWEP.AttachmentModelsVM = {
 	    ["too_reflex_mrs"] = { type = "Model", model = "models/eftatts/eft_scope_mrs.mdl", bone = "weapon", rel = "", pos = Vector(0.079, -3.264, 0.981), angle = Angle(90, 0, -90), size = Vector(1, 1, 1), color = Color(255, 255, 255, 255), surpresslightning = false, material = "", skin = 0, bodygroup = {} },
 		["too_reflex_exps"] = { type = "Model", model = "models/eftatts/eft_scope_exps.mdl", bone = "weapon", rel = "", pos = Vector(0, -3.165, 2), angle = Angle(90, 0, -90), size = Vector(1, 1, 1), color = Color(255, 255, 255, 255), surpresslightning = false, material = "", skin = 0, bodygroup = {} },
-		["md_aimpoint"] = { type = "Model", model = "models/wystan/attachments/aimpoint.mdl", bone = "weapon", rel = "", pos = Vector(-0.16, 2.474, -5.295), angle = Angle(0, 0, -90), size = Vector(1, 1, 1), color = Color(255, 255, 255, 255), surpresslightning = false, material = "", skin = 0, bodygroup = {} },
-		["md_foregrip"] = {model = "models/wystan/attachments/foregrip1.mdl", bone = "ak74_body", pos = Vector(4.151, -0.433, -2.721), angle = Angle(0, -90, 0), size = Vector(0.75, 0.75, 0.75)},
+		["too_reflex_romeo4t"] = { type = "Model", model = "models/eftatts/eft_scope_romeo4t.mdl", bone = "weapon", rel = "", pos = Vector(0, -3.165, 2), angle = Angle(90, 0, -90), size = Vector(1, 1, 1), color = Color(255, 255, 255, 255), surpresslightning = false, material = "", skin = 0, bodygroup = {} },
+		["md_aimpoint"] = { type = "Model", model = "models/wystan/attachments/aimpoint.mdl", bone = "weapon", rel = "", pos = Vector(-0.16, 2.474, 0), angle = Angle(0, 0, -90), size = Vector(1, 1, 1), color = Color(255, 255, 255, 255), surpresslightning = false, material = "", skin = 0, bodygroup = {} },
 		["md_pbs1"] = { type = "Model", model = "models/cw2/attachments/pbs1.mdl", bone = "weapon", rel = "", pos = Vector(0.079, -2.274, 24.422), angle = Angle(0, 0, 90), size = Vector(1, 1, 1), color = Color(255, 255, 255, 255), surpresslightning = false, material = "", skin = 0, bodygroup = {} },
 		["md_kobra"] = { type = "Model", model = "models/cw2/attachments/kobra.mdl", bone = "weapon", rel = "", pos = Vector(0.479, 1.35, 0.842), angle = Angle(180, 0, -90), size = Vector(0.699, 0.699, 0.699), color = Color(255, 255, 255, 255), surpresslightning = false, material = "", skin = 0, bodygroup = {} },
 		["md_pso1"] = { type = "Model", model = "models/cw2/attachments/pso.mdl", bone = "weapon", rel = "", pos = Vector(0, 0.462, -3.254), angle = Angle(180, 0, -90), size = Vector(0.85, 0.85, 0.85), color = Color(255, 255, 255, 255), surpresslightning = false, material = "", skin = 0, bodygroup = {} },
@@ -75,7 +78,7 @@ SWEP.LuaViewmodelRecoil = true
 SWEP.SightBGs = {main = 1, carryhandle = 0, none = 1}
 SWEP.StockBGs = {main = 2, foldable = 1, regular = 0}
 
-SWEP.Attachments = {[1] = {header = "Sight", offset = {950, -500},  atts = {"md_kobra","too_reflex_mrs", "too_reflex_exps", "md_aimpoint", "md_schmidt_shortdot", "md_pso1", "md_acog"}},
+SWEP.Attachments = {[1] = {header = "Sight", offset = {950, -500},  atts = {"md_kobra","too_reflex_mrs","too_reflex_romeo4t", "too_reflex_exps", "md_aimpoint", "md_schmidt_shortdot", "md_pso1", "md_acog"}},
 	[2] = {header = "Barrel", offset = {300, -500}, atts = {"md_pbs1"}},
 	[3] = {header = "Stock", offset = {700, 500}, atts = {"too_bg_74stock"}},
 	["+use"] = {header = "Optic Type", offset = {0, 0}, atts = {"too_optic_category_reflex", "too_optic_category_magnified"}},
@@ -91,6 +94,7 @@ SWEP.Attachments = {[1] = {header = "Sight", offset = {950, -500},  atts = {"md_
 		["md_schmidt_shortdot"] = {"too_optic_category_magnified"},
 		["md_pso1"] = {"too_optic_category_magnified"},
 		["too_reflex_mrs"] = {"too_optic_category_reflex"},
+		["too_reflex_romeo4t"] = {"too_optic_category_reflex"},
 	}
 	
 SWEP.Animations = {
