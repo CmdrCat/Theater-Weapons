0b787f89213a03df8c4014b0313855c5eb116a3c
CmdrCat <cgifaction@gmail.com>
2025-09-07 19:17:39 -0500

    Forgot to save ammo modifiers for MP5A5

--- lua/weapons/cw_theater_mp5/shared.lua
+++ lua/weapons/cw_theater_mp5/shared.lua
@@ -242,4 +242,10 @@ if (self.ActiveAttachments.too_sdbar_bar) then
 self.EffectiveRange = ((self.EffectiveRange - 15 * 39.37))
 self.DamageFallOff = ((self.DamageFallOff + 0.3))
 end
+if self.ActiveAttachments.am_magnum then
+	self.EffectiveRange = ((self.EffectiveRange * 1.15))
+end
+if self.ActiveAttachments.am_matchgrade then
+	self.DamageFallOff = ((self.DamageFallOff * 0.5))
+end
 end
