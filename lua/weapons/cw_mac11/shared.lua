8cbaf254b6515131950a45f23d3a016d3ed78650
CmdrCat <cgifaction@gmail.com>
2025-10-14 17:34:08 -0500

    Changed MAC-11 customization UI, excluded RR rounds from conversion

--- lua/weapons/cw_mac11/shared.lua
+++ lua/weapons/cw_mac11/shared.lua
@@ -94,11 +94,13 @@ SWEP.LuaViewmodelRecoil = true
 
 SWEP.Trivia = {text = "This submachine gun has a monstrous rate of fire; one so fast that many users find it disadvantageous.", x = -250, y = -450}
 
+SWEP.AttachmentExclusions = {["am_atow_lowvel"] = {"cw_mac11_380_conversion"}}
+
 SWEP.Attachments = {[1] = {header = "Sight", offset = {800, -300},  atts = {"md_microt1"}},
 	[2] = {header = "Muzzle", offset = {-500, -600},  atts = {"md_tundra9mm"}},
 	[3] = {header = "Barrel", offset = {-500, -150},  atts = {"bg_mac11_extended_barrel"}},
-	[4] = {header = "Stock", offset = {800, 150},  atts = {"bg_mac11_unfolded_stock"}},
-	[5] = {header = "Conversion", offset = {800, 150},  atts = {"cw_mac11_380_conversion"}},
+	[4] = {header = "Stock", offset = {1100, 100},  atts = {"bg_mac11_unfolded_stock"}},
+	[5] = {header = "Conversion", offset = {700, 300},  atts = {"cw_mac11_380_conversion"}},
 	["+reload"] = {header = "Ammo", offset = {-500, 300}, atts = {"am_magnum", "am_matchgrade", "am_atow_lowvel", "am_atow_heavy", "am_atow_ap"}}}
 
 SWEP.Animations = {fire = {"mac10_fire", "mac10_fire2", "mac10_fire3"},
