0ebd06799b5e0e2d90035bdb20e6f31f059acd62
CmdrCat <cgifaction@gmail.com>
2026-05-31 13:10:34 -0500

    New Limb Damage Multiplier Code
    Added new, original limb damage multiplier code (thanks zynx). This keeps the same multipliers, but only for CW guns. No longer has unique damage multipliers for NPCs

--- lua/autorun/limb_damage_multiplier.lua
+++ lua/autorun/limb_damage_multiplier.lua
@@ -1,3 +1,20 @@
+hook.Add( "PreRegisterSWEP", "cw_mult", function( swep )
+    if swep.Base ~= "cw_base" then return end
+
+    swep.bulletCallback = function( att, tr, dmg )
+        if tr.HitGroup == HITGROUP_HEAD then
+            dmg:ScaleDamage( 0.75 ) -- Default headshot damage multiplier is 2, so we're multiplying 2 by the value here (0.75) to get 1.5
+        end
+		if (tr.HitGroup == HITGROUP_LEFTARM || tr.HitGroup == HITGROUP_RIGHTARM) then
+			dmg:ScaleDamage( 4 ) -- Default arm damage multiplier is 0.25, so we're multiplying 0.25 by the value here (4) to get 1
+		end
+		if (tr.HitGroup == HITGROUP_LEFTLEG || tr.HitGroup == HITGROUP_RIGHTLEG) then
+			dmg:ScaleDamage( 3 ) -- Default leg damage multiplier is 0.25, so we're multiplying 0.25 by the value here (3) to get 0.75
+		end
+    end
+end )
+
+/*
 function PlayerDMGMult(self, ply, hitgroup, dmginfo)
 	local PlayerDamageMul = 1
 	if (hitgroup == HITGROUP_HEAD) then
@@ -33,3 +50,4 @@ hook.Add("PostGamemodeLoaded", "limb_damage_multiplier", ReplaceHook)
 hook.Add("OnReloaded", "limb_damage_multiplier", function()
 	timer.Simple(0, ReplaceHook)
 end)
+*/
