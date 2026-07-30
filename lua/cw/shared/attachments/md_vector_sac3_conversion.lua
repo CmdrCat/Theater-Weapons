198eda1a468ff179bd335fa7a9c390a77ee03a07
CmdrCat <cgifaction@gmail.com>
2026-07-30 18:35:07 -0500

    oops

--- lua/cw/shared/attachments/md_vector_sac3_conversion.lua
+++ lua/cw/shared/attachments/md_vector_sac3_conversion.lua
@@ -11,11 +11,12 @@ if CLIENT then
 	att.displayIcon = surface.GetTextureID("atts/fullautorec")
 	att.description = {[1] = {t = "Steady-shooting variant dependent on whether it is dual / single-wielded. ", c = CustomizableWeaponry.textColors.COSMETIC},
 					   [2] = {t = "Increases headshot multiplier to 1.1x", c = CustomizableWeaponry.textColors.POSITIVE},
-					   [3] = {t = "Increases damage to 50 / 32", c = CustomizableWeaponry.textColors.POSITIVE},
-					   [4] = {t = "Decreases firerate to 600 / 923 RPM", c = CustomizableWeaponry.textColors.NEGATIVE},
-					   [5] = {t = "Increases effective range to 31 / 22 M", c = CustomizableWeaponry.textColors.POSITIVE},
-					   [6] = {t = "Decreases damage fall off to 28 / 37%", c = CustomizableWeaponry.textColors.POSITIVE},
-					   [7] = {t = "Decreases reload speed to 1.5 / 2 s", c = CustomizableWeaponry.textColors.POSITIVE}}
+					   [3] = {t = "Decreases magazine capacity to 32 rounds", c = CustomizableWeaponry.textColors.NEGATIVE},
+					   [4] = {t = "Increases damage to 50 / 32", c = CustomizableWeaponry.textColors.POSITIVE},
+					   [5] = {t = "Decreases firerate to 600 / 923 RPM", c = CustomizableWeaponry.textColors.NEGATIVE},
+					   [6] = {t = "Increases effective range to 31 / 22 M", c = CustomizableWeaponry.textColors.POSITIVE},
+					   [7] = {t = "Decreases damage fall off to 28 / 37%", c = CustomizableWeaponry.textColors.POSITIVE},
+					   [8] = {t = "Decreases reload speed to 1.5 / 2 s", c = CustomizableWeaponry.textColors.POSITIVE}}
 end
 
 function att:attachFunc()
@@ -23,6 +24,9 @@ function att:attachFunc()
 	self.PrintName = "SAC3"
 	self.FireSound = "CW_MP9_FIRE"
 	self.FireSoundSuppressed = "MWII_M13B_FIRE_SUPPRESSED"
+
+	self.Primary.ClipSize = 32
+	self.Primary.ClipSize_Orig = 32
 
 	if SERVER then
 		return
@@ -39,6 +43,9 @@ function att:detachFunc()
 	self.PrintName = "Vector"
 	self.FireSound = "CW_VECTOR_FIRE"
 	self.FireSoundSuppressed = "DOITHOM1928_FIRE_SUPPRESSED"
+
+	self.Primary.ClipSize = self.Primary.ClipSize_ORIG_REAL
+	self.Primary.ClipSize_Orig = self.Primary.ClipSize_ORIG_REAL
 
 	if SERVER then
 		return
