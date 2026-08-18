6acdad583c374954fbccf025fd9fa18bb4d25f1b
Mattunderwear0312 <91106060+Mattunderwear0312@users.noreply.github.com>
2026-08-18 18:53:56 -0400

    Fixed MCX
    MCX extra sounds attached to sh_sounds/shared.lua removed

--- lua/weapons/cwr_mwii_mcharliex/sh_sounds.lua
+++ lua/weapons/cwr_mwii_mcharliex/sh_sounds.lua
@@ -2,7 +2,3 @@
 
 CustomizableWeaponry:addFireSound("MWII_M13B_FIRE", "weapons/r_mw/m13b/fire_fp.wav", 1, 115, CHAN_WEAPON)
 CustomizableWeaponry:addFireSound("MWII_M13B_FIRE_SUPPRESSED", "weapons/r_mw/m13b/fire_sil.wav", 1, 90, CHAN_WEAPON)
-
-CustomizableWeaponry:addFireSound("mcxfire", "weapons_too/mcharliex/fire_fp.wav", 1, 115, CHAN_WEAPON)
-CustomizableWeaponry:addFireSound("mcxfiresupp", "weapons_too/mcharliex/fire_sil.wav", 1, 90, CHAN_WEAPON)
-
