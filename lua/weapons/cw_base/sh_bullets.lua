c3534242021afc93c6e1904cee0039f39ec9d40a
Mattunderwear0312 <91106060+Mattunderwear0312@users.noreply.github.com>
2026-08-31 21:42:11 -0400

    Fake hits fix.

--- lua/weapons/cw_base/sh_bullets.lua
+++ lua/weapons/cw_base/sh_bullets.lua
@@ -1,17 +1,17 @@
-local Dir, Dir2, dot, sp, ent, trace, seed, hm
-
 SWEP.NormalTraceMask = bit.bor(CONTENTS_SOLID, CONTENTS_OPAQUE, CONTENTS_MOVEABLE, CONTENTS_DEBRIS, CONTENTS_MONSTER, CONTENTS_HITBOX, 402653442, CONTENTS_WATER)
 SWEP.WallTraceMask = bit.bor(CONTENTS_TESTFOGVOLUME, CONTENTS_EMPTY, CONTENTS_MONSTER, CONTENTS_HITBOX)
 
 SWEP.NoPenetration = {[MAT_SLOSH] = true}
 SWEP.NoRicochet = {[MAT_FLESH] = true, [MAT_ANTLION] = true, [MAT_BLOODYFLESH] = true, [MAT_DIRT] = true, [MAT_SAND] = true, [MAT_GLASS] = true, [MAT_ALIENFLESH] = true, [MAT_GRASS] = true}
 SWEP.PenetrationMaterialInteraction = {[MAT_SAND] = 0.5, [MAT_DIRT] = 0.8, [MAT_METAL] = 1.1, [MAT_TILE] = 0.9, [MAT_WOOD] = 1.2}
-local bul, tr = {}, {}
-local SP = game.SinglePlayer()
 local zeroVec = Vector(0, 0, 0)
 
 local reg = debug.getregistry()
 local GetShootPos = reg.Player.GetShootPos
+
+-- Optional API only: the separate HitReg addon owns all recovery traces and
+-- damage dispatch. Without it, CW2 uses its normal Source bullet callback.
+SWEP.TCWHitRegBridgeVersion = 1
 
 SWEP.bulletCallback = function(ply, traceResult, dmgInfo) 
 	CustomizableWeaponry.callbacks.processCategory(ply:GetActiveWeapon(), "bulletCallback", ply, traceResult, dmgInfo)
@@ -22,9 +22,9 @@ function SWEP:canPenetrate(traceData, direction)
 	
 	if not self.NoPenetration[traceData.MatType] then
 		dot = self:getSurfaceReflectionDotProduct(traceData, direction)
-		ent = traceData.Entity
+		local ent = traceData.Entity
 	
-		if not ent:IsNPC() and not ent:IsPlayer() then
+		if not ent:IsNPC() and not ent:IsPlayer() and not ent:IsNextBot() then
 			if dot > 0.26 and self.CanPenetrate then
 				return true, dot
 			end
@@ -49,24 +49,43 @@ function SWEP:canRicochet(traceData, penetrativeRange)
 end
 
 function SWEP:FireBullet(damage, cone, clumpSpread, bullets)
-	sp = GetShootPos(self.Owner)
+	-- Shot-local state also survives a callback firing a different weapon.
+	local bul, tr = {}, {}
+	local trace, dot
+	local sp = GetShootPos(self.Owner)
 	local commandNumber = self.Owner:GetCurrentCommand():CommandNumber()
-	math.randomseed(commandNumber)
 	
 	if self.Owner:Crouching() then
 		cone = cone * 0.85
 	end
 	
-	Dir = (self.Owner:EyeAngles() + self.Owner:GetViewPunchAngles() + Angle(math.Rand(-cone, cone), math.Rand(-cone, cone), 0) * 25):Forward()
+	local Dir = (self.Owner:EyeAngles() + self.Owner:GetViewPunchAngles() + Angle(
+		util.SharedRandom("TCW.Spread.Pitch", -cone, cone, commandNumber),
+		util.SharedRandom("TCW.Spread.Yaw", -cone, cone, commandNumber), 0) * 25):Forward()
 	clumpSpread = clumpSpread or self.ClumpSpread
+	bul.Attacker = self.Owner
+	bul.AmmoType = self.Primary.Ammo
+	bul.wep = self
+
+	local originalCallback = self.bulletCallback
+	local function shotCallback(attacker, hitTrace, dmgInfo)
+		local hitreg = SERVER and TheaterPlayerHitReg
+		if hitreg and isfunction(hitreg.ProcessCW2Bullet) then
+			return hitreg:ProcessCW2Bullet(self, attacker, hitTrace, dmgInfo, originalCallback)
+		end
+		if originalCallback then return originalCallback(attacker, hitTrace, dmgInfo) end
+	end
 	
 	CustomizableWeaponry.callbacks.processCategory(self, "adjustBulletStructure", bul)
 	
 	for i = 1, bullets do
-		Dir2 = Dir
+		local Dir2 = Dir
 		
 		if clumpSpread and clumpSpread > 0 then
-			Dir2 = Dir + Vector(math.Rand(-1, 1), math.Rand(-1, 1), math.Rand(-1, 1)) * clumpSpread
+			Dir2 = Dir + Vector(
+				util.SharedRandom("TCW.Pellet.X", -1, 1, commandNumber + i),
+				util.SharedRandom("TCW.Pellet.Y", -1, 1, commandNumber + i),
+				util.SharedRandom("TCW.Pellet.Z", -1, 1, commandNumber + i)) * clumpSpread
 		end
 		
 		if not CustomizableWeaponry.callbacks.processCategory(self, "suppressDefaultBullet", sp, Dir2, commandNumber) then
@@ -78,7 +97,7 @@ function SWEP:FireBullet(damage, cone, clumpSpread, bullets)
 			bul.TracerName = self.TracerName
 			bul.Force	= damage * 0.3
 			bul.Damage = math.Round(damage)
-			bul.Callback = self.bulletCallback
+			bul.Callback = shotCallback
 			bul.wep = self
 			
 			self.Owner:FireBullets(bul)
@@ -112,7 +131,7 @@ function SWEP:FireBullet(damage, cone, clumpSpread, bullets)
 						bul.Num = 1
 						bul.Src = trace.HitPos
 						bul.Dir = Dir2
-						bul.Spread 	= Vec0
+						bul.Spread 	= zeroVec
 						bul.Tracer	= 4
 						bul.Force	= damage * 0.15
 						bul.Damage = bul.Damage * 0.5
@@ -122,7 +141,7 @@ function SWEP:FireBullet(damage, cone, clumpSpread, bullets)
 						bul.Num = 1
 						bul.Src = trace.HitPos
 						bul.Dir = -Dir2
-						bul.Spread 	= Vec0
+						bul.Spread 	= zeroVec
 						bul.Tracer	= 4
 						bul.Force	= damage * 0.15
 						bul.Damage = bul.Damage * 0.5
@@ -133,12 +152,15 @@ function SWEP:FireBullet(damage, cone, clumpSpread, bullets)
 					if self:canRicochet(trace) then
 						dot = dot or self:getSurfaceReflectionDotProduct(trace, Dir2)
 						Dir2 = Dir2 + (trace.HitNormal * dot) * 3
-						math.randomizeVector(Dir2, 0.06)
+						Dir2 = Dir2 + Vector(
+							util.SharedRandom("TCW.Ricochet.X", -0.06, 0.06, commandNumber + i),
+							util.SharedRandom("TCW.Ricochet.Y", -0.06, 0.06, commandNumber + i),
+							util.SharedRandom("TCW.Ricochet.Z", -0.06, 0.06, commandNumber + i))
 						
 						bul.Num = 1
 						bul.Src = trace.HitPos
 						bul.Dir = Dir2
-						bul.Spread 	= Vec0
+						bul.Spread 	= zeroVec
 						bul.Tracer	= 0
 						bul.Force	= damage * 0.225
 						bul.Damage = bul.Damage * 0.75
